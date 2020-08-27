b9efd8254fec262150e6bea4147d840008ac0532
DustyBest <dustybest@gmail.com>
2020-08-27 14:47:48 -0700

    mapBox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiZHVzdHliZXN0IiwiYSI6ImNrZTh1Zms3aDE4ZzgzMG9hMGpmMzlqdnYifQ.l9X3R4Pg6NVQYtM5EFo40A'
 };
